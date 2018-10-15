2d0df9030e0d4db0f58b0541a57caa070a0edc9d
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2018-10-14 20:40:45 -0700

    Added click CLI.

--- motion_view/__main__.py
+++ motion_view/__main__.py
@@ -4,8 +4,15 @@ import pims
 import click
 import numpy as np
 
+@click.group()
+def cli():
+    pass
 
-def background(images):
+
+@cli.command()
+@click.argument('path', click.Path(exists=True))
+def background(path):
+    images = pims.Video(str(path))
     images = np.stack(images)
     median = np.median(images, axis=0)
     median = median.astype(int)
@@ -15,21 +22,18 @@ def background(images):
     io.imsave('maxima.png', maxima)
     io.imsave('median.png', median)
 
-def motion_image(images):
+@cli.command()
+@click.argument('path', click.Path(exists=True))
+@click.option('--indices', '-i', type=int, multiple=True, default=(30, 40, 50))
+def motion_image(path, indices):
+    images = pims.Video(str(path))
     bg = io.imread('median.png')
     white = np.ones(bg.shape)*255
     new_frame = bg * .8
-    for idx, frame in enumerate(images):
-        if idx%10!=5: continue
-        # new_frame = frame-bg
-
+    for idx in indices:
+        frame = images[idx]
         new_frame = np.where((np.max(np.abs(frame-bg), axis=2)>20)[:, :, np.newaxis] , frame, new_frame )
-    io.imsave(f'{idx}.png', new_frame.astype('uint8'))
+    io.imsave('composite.png', new_frame.astype('uint8'))
 
 if __name__ == '__main__':
-    import sys
-    import pathlib
-    path = pathlib.Path(sys.argv[1])
-    images = pims.Video(str(path))
-    motion_image(images)
-    # background(images)
+    cli()
